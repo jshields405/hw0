6bd4010c8daf8c2440f6e63fcde45b30639a644c
Jack Shields <jshields405@gmail.com>
2020-08-28 01:18:40 +0000

    got rid of 'a'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-temp = input("Enter a temperature in celsius: ")
+temp = input("Enter temperature in celsius:")
 temp = float(temp)
 fahr = ((temp*9)/5)+32
 print(f"{temp}° in Celsius is equivalent to {fahr}° Fahrenheit.")
